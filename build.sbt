1d707484fb15dd70b907dcc539de0808df22736b
David Hall <dlwh@cs.berkeley.edu>
2014-07-26 11:44:23 -0700

    towards the new setup

--- build.sbt
+++ build.sbt
@@ -5,7 +5,12 @@ version := "1.0"
 libraryDependencies ++= Seq(
   "edu.berkeley.nlp.cs" %% "igor" % "0.1-SNAPSHOT",
   "edu.berkeley.nlp.cs" %% "erector" % "0.1-SNAPSHOT",
+  "org.scalanlp" %% "breeze" % "0.9-SNAPSHOT",
   "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.0-beta9",
   "org.apache.logging.log4j" % "log4j-core" % "2.0-beta9",
   "org.apache.logging.log4j" % "log4j-api" % "2.0-beta9"
 )
+
+fork := true
+
+javaOptions := Seq("-Xmx6g", "-Xrunhprof:cpu=samples,depth=12", "-Dlog4j.configurationFile=src/main/resources/log4j.xml")
--- src/main/scala/lllm/main/PrecomputeFeatures.scala
+++ src/main/scala/lllm/main/PrecomputeFeatures.scala
@@ -1,16 +1,15 @@
 package lllm.main
 
-import breeze.stats.distributions.Uniform
-import igor.experiment.{ResultCache, Experiment, Stage}
-import erector.corpus.TextCorpusReader
-import lllm.features._
-import erector.util.text.toNGramIterable
-import breeze.linalg.{sum, Counter}
-import lllm.model.UnigramLanguageModel
+import breeze.linalg.Counter
+import breeze.stats.distributions.Multinomial
 import breeze.util.Index
-import breeze.features.FeatureVector
-import lllm.util.HuffmanDict
+import erector.corpus.TextCorpusReader
+import erector.util.text.toNGramIterable
+import igor.experiment.{ResultCache, Stage}
+import lllm.features._
 import lllm.model.ObjectiveType._
+import lllm.model.UnigramLanguageModel
+import lllm.util.HuffmanDict
 
 /**
  * @author jda
@@ -18,8 +17,6 @@ import lllm.model.ObjectiveType._
 object PrecomputeFeatures extends Stage[LLLMParams] {
 
   override def run(config: LLLMParams, cache: ResultCache): Unit = {
-
-    val includeWordIdentityInFeature = config.objectiveType != Hierarchical
 
     val contextFeaturizer = NGramFeaturizer(2, config.order) + WordAndIndexFeaturizer() + ConstantFeaturizer()
     val predictionFeaturizer = IdentityFeaturizer()
@@ -31,16 +28,14 @@ object PrecomputeFeatures extends Stage[LLLMParams] {
     val corpus = TextCorpusReader(config.trainPath)
     val contextFeatureCounts = Counter(corpus.nGramIterator(config.order).flatMap { nGram => contextFeaturizer(nGram).map((_,1)) })
     val positiveFeatIndex = Index(corpus.nGramFeatureIndexer(config.order, contextFeaturizer).filter(contextFeatureCounts(_) > 5))
-    val featIndex =
-      if (config.useHashing)
-        HashingFeatureIndex(positiveFeatIndex)
-      else
-        positiveFeatIndex
+    val featIndex = positiveFeatIndex
 
     cache.putDisk('ContextFeaturizer, contextFeaturizer)
     cache.putDisk('ContextFeatureIndex, featIndex)
     cache.putDisk('PredictionFeaturizer, predictionFeaturizer)
     cache.putDisk('VocabIndex, corpus.vocabularyIndex)
+
+    val cpIndexBuilder = new CrossProductIndex.Builder(corpus.vocabularyIndex, featIndex, hashFeatures = if(config.useHashing) 1.0 else 0.0)
 
     val counts = Counter[String,Double](corpus.nGramIterator(1).map(_(0) -> 1d))
     val noiseDistribution = Multinomial[Counter[String,Double],String](counts)
@@ -77,14 +72,18 @@ object PrecomputeFeatures extends Stage[LLLMParams] {
             val predFeats = predictionFeaturizer(ngram)
             // for now we are assuming these are the same thing (see comment above)
             assert(predFeats.size == 1 && predFeats.last == prediction)
-            (predFeats map corpus.vocabularyIndex, samplingDistribution.probabilityOf(prediction))
+            ((predFeats map corpus.vocabularyIndex).toArray, samplingDistribution.probabilityOf(prediction))
           }).unzip
+
+          for( (context, data) <- contextFeatures zip predictionFeatures) {
+            cpIndexBuilder.add(data, context)
+          }
 
           val (noiseFeatures, noiseProbs) = (batchNGrams map { ngram: IndexedSeq[String] =>
             val samples = samplingDistribution.sample(config.noiseSamples)
             (samples map { sample =>
               val predFeats = predictionFeaturizer(IndexedSeq(sample))
-              (predFeats map corpus.vocabularyIndex, samplingDistribution.probabilityOf(sample))
+              ((predFeats map corpus.vocabularyIndex).toArray, samplingDistribution.probabilityOf(sample))
             }).unzip
           }).unzip
 
@@ -100,6 +99,9 @@ object PrecomputeFeatures extends Stage[LLLMParams] {
       }
     }
 
+
+
     cache.putDisk('NLineGroups, Int.box(corpus.lineGroupIterator(config.featureGroupSize).length))
+    cache.putDisk('CrossIndex, cpIndexBuilder.result())
   }
 }
